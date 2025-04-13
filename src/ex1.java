136dfc46136b47fb50ee15da48301946b8db483d
david <cdtjfh@gmail.com>
2025-04-14 07:58:22 +0900

    Drawshape

--- src/ex1.java
+++ src/ex1.java
@@ -1,23 +1,7 @@
 import java.util.Scanner;
 
 
-/*
-float divide(int x, int y) { // 작업하기 전 나누는 수 y가 0인지 확인한다.
-    if(y==0) {
-    s.out.println("0으로 나눌 수 없다.")
-    return 0; // 메서드 종료
-    }
-    return x / (int) y;
-    }
-    두번째 입력값이 0이 되면 안됨
-    if(num2==0)
-    break;
-
-
- */
-
-
-public class calculate {
+public class ex1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
